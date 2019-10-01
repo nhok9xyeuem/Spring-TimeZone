18a9853b8655cfe02064b91d4590c9a3fd716fce
nhok9xyeuem <nhuconghaui@gmail.com>
2019-10-01 14:10:45 +0700

    update

--- src/main/java/controllers/TimeZoneControllers.java
+++ src/main/java/controllers/TimeZoneControllers.java
@@ -17,14 +17,20 @@ public class TimeZoneControllers {
                                             required = false,
                                             defaultValue = "Asia/Ho_Chi_Minh")
                                             String city) {
+        // Get current time at local
         Date date = new Date();
+// Get timezone by the local
         TimeZone local = TimeZone.getDefault();
-        TimeZone locale = TimeZone.getTimeZone("city");
-        long local_time = date.getTime() + (locale.getRawOffset() -local.getRawOffset());
-
+// Get timezone by the specified city
+        TimeZone locale = TimeZone.getTimeZone(city);
+        // Calculate the current time in the specified city
+        long locale_time = date.getTime() +
+                (locale.getRawOffset() - local.getRawOffset());
+// Reset the date by locale_time
+        date.setTime(locale_time);
+        // Set the data sent to the view
         model.addAttribute("city", city);
         model.addAttribute("date", date);
-
         return "display";
 
     }
